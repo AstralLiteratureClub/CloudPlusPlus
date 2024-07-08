d100b09d517e69ad293d7ab2f62660d2813756ed
Antritus <antt.graft@gmail.com>
2024-07-08 17:37:35 +0300

    1.2.5

--- src/main/java/bet/astral/cloudplusplus/command/CPPCommand.java
+++ src/main/java/bet/astral/cloudplusplus/command/CPPCommand.java
@@ -9,11 +9,14 @@ import org.incendo.cloud.Command;
 import org.incendo.cloud.CommandManager;
 import org.incendo.cloud.description.Description;
 import org.incendo.cloud.minecraft.extras.RichDescription;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Locale;
 import java.util.function.Function;
 
+@SuppressWarnings("unused")
 public class CPPCommand<C> {
 	protected final CommandManager<C> commandManager;
 	protected final CommandRegisterer<C> registerer;
@@ -40,10 +43,10 @@ public class CPPCommand<C> {
 				builder);
 	}
 	public RegistrableCommand<? extends C> command(@NotNull String name,
-	                                    @NotNull String[] args,
+	                                    @NotNull String[] aliases,
 	                                    @NotNull TranslationKey descriptionKey,
 	                                    @NotNull Function<Command.Builder<C>, Command.Builder<? extends C>> builder){
-		return command(name, args, loadDescription(descriptionKey), builder);
+		return command(name, aliases, description(descriptionKey), builder);
 	}
 
 	public RegistrableCommand<? extends C> command(@NotNull String name,
@@ -56,89 +59,111 @@ public class CPPCommand<C> {
 				builder);
 	}
 	public RegistrableCommand<? extends C> command(@NotNull String name,
-	                                     @NotNull String[] args,
+	                                     @NotNull String[] aliases,
 	                                     @NotNull Description description,
 	                                     @NotNull Function<Command.Builder<C>, Command.Builder<? extends C>> builder){
-		Command.Builder<C> bldr = commandManager.commandBuilder(name, description, args).commandDescription(description);
+		Command.Builder<C> bldr = commandManager.commandBuilder(name, description, aliases).commandDescription(description);
 		return new RegistrableCommand<>(commandManager, builder.apply(bldr));
 	}
-	public RegistrableCommand<? extends C> command(Command.Builder<C> source, @NotNull String name,
+	public RegistrableCommand<? extends C> command(Command.Builder<C> source, @Nullable String name,
 	                                               @NotNull TranslationKey descriptionKey,
 	                                               @NotNull Function<Command.Builder<C>, Command.Builder<? extends C>> builder,
-	                                               @NotNull String... aliases){
+	                                               @Nullable String... aliases){
 		return command(source,
 				name,
 				aliases,
 				descriptionKey,
 				builder);
 	}
-	public RegistrableCommand<? extends C> command(Command.Builder<C> source, @NotNull String name,
-	                                               @NotNull String[] args,
+	public RegistrableCommand<? extends C> command(Command.Builder<C> source, @Nullable String name,
+	                                               @Nullable String[] aliases,
 	                                               @NotNull TranslationKey descriptionKey,
 	                                               @NotNull Function<Command.Builder<C>, Command.Builder<? extends C>> builder){
-		return command(source, name, args, loadDescription(descriptionKey), builder);
+		return command(source, name, aliases, description(descriptionKey), builder);
 	}
 
-	public RegistrableCommand<? extends C> command(Command.Builder<C> source, @NotNull String name,
+	public RegistrableCommand<? extends C> command(Command.Builder<C> source, @Nullable String name,
 	                                               @NotNull Description description,
 	                                               @NotNull Function<Command.Builder<C>, Command.Builder<? extends C>> builder,
-	                                               @NotNull String... aliases){
+	                                               @Nullable String... aliases){
 		return command(source, name,
 				aliases,
 				description,
 				builder);
 	}
-	public RegistrableCommand<? extends C> command(Command.Builder<C> source, @NotNull String name,
-	                                               @NotNull String[] args,
+	public RegistrableCommand<? extends C> command(Command.Builder<C> source, @Nullable String name,
+	                                               @Nullable String[] aliases,
 	                                               @NotNull Description description,
 	                                               @NotNull Function<Command.Builder<C>, Command.Builder<? extends C>> builder){
-		Command.Builder<C> bldr = source.literal(name, description, args).commandDescription(description);
+		if (name != null){
+			if (aliases != null){
+				source = source.literal(name, description, aliases);
+			} else {
+				source = source.literal(name, description);
+			}
+		}
+		Command.Builder<C> bldr = source.commandDescription(description);
 		return new RegistrableCommand<>(commandManager, builder.apply(bldr));
 	}
 
-	public RegistrableCommand<? extends C> command(RegistrableCommand<? extends C> command, @NotNull String name,
+	public RegistrableCommand<? extends C> command(RegistrableCommand<? extends C> command, @Nullable String name,
 	                                               @NotNull TranslationKey descriptionKey,
 	                                               @NotNull Function<Command.Builder<C>, Command.Builder<? extends C>> builder,
-	                                               @NotNull String... aliases){
+	                                               @Nullable String... aliases){
 		return command(command,
 				name,
 				aliases,
 				descriptionKey,
 				builder);
 	}
-	public RegistrableCommand<? extends C> command(RegistrableCommand<? extends C> command, @NotNull String name,
-	                                               @NotNull String[] args,
+	public RegistrableCommand<? extends C> command(RegistrableCommand<? extends C> command, @Nullable String name,
+	                                               @Nullable String[] aliases,
 	                                               @NotNull TranslationKey descriptionKey,
 	                                               @NotNull Function<Command.Builder<C>, Command.Builder<? extends C>> builder){
-		return command(command, name, args, loadDescription(descriptionKey), builder);
+		return command(command, name, aliases, description(descriptionKey), builder);
 	}
 
-	public RegistrableCommand<? extends C> command(RegistrableCommand<? extends C> command, @NotNull String name,
+	public RegistrableCommand<? extends C> command(RegistrableCommand<? extends C> command, @Nullable String name,
 	                                               @NotNull Description description,
 	                                               @NotNull Function<Command.Builder<C>, Command.Builder<? extends C>> builder,
-	                                               @NotNull String... aliases){
+	                                               @Nullable String... aliases){
 		return command(command, name,
 				aliases,
 				description,
 				builder);
 	}
-	public RegistrableCommand<? extends C> command(RegistrableCommand<? extends C> command, @NotNull String name,
-	                                               @NotNull String[] args,
+	public RegistrableCommand<? extends C> command(RegistrableCommand<? extends C> command, @Nullable String name,
+	                                               @Nullable String[] aliases,
 	                                               @NotNull Description description,
 	                                               @NotNull Function<Command.Builder<C>, Command.Builder<? extends C>> builder){
-		Command.Builder<C> bldr = (Command.Builder<C>) command.builder.literal(name, description, args).commandDescription(description);
-		return new RegistrableCommand<>(commandManager, builder.apply(bldr));
+		Command.Builder<? extends C> src = command.getBuilder();
+		if (name != null){
+			if (aliases != null){
+				//noinspection NullableProblems
+				src = src.literal(name, description, aliases);
+			} else {
+				src = src.literal(name, description);
+			}
+		}
+		Command.Builder<? extends C> bldr = src.commandDescription(description);
+		//noinspection unchecked
+		return new RegistrableCommand<>(commandManager, builder.apply((Command.Builder<C>) bldr));
 	}
 
 
 
 
-	public RichDescription loadDescription(@NotNull TranslationKey translationKey){
+	public RichDescription description(@NotNull TranslationKey translationKey){
 		Component component = messenger.parseComponent(translationKey, locale, ComponentType.CHAT);
 		if (component == null){
 			return RichDescription.translatable(translationKey.getKey());
 		}
 		return RichDescription.of(component);
+	}
+	@ApiStatus.ScheduledForRemoval(inVersion = "1.3.0")
+	@Deprecated
+	public RichDescription loadDescription(@NotNull TranslationKey translationKey){
+		return description(translationKey);
 	}
 
 	public static class RegistrableCommand<C> {
